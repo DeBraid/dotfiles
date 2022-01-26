525cc6eef736c0d44ae148cf26a08ccfdc2dcadd
Derek Braid <dbshop@dbshops-MacBook-Pro.local>
2022-01-26 16:01:16 -0500

    put source in proper file

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 # ln -sf ~/dotfiles/zshrc ~/.zshrc
 cat ~/dotfiles/zshrc >> ~/.zshrc
 
-curl -L https://iterm2.com/shell_integration/zsh \
--o ~/.iterm2_shell_integration.zsh
-# Next, you need to load the script at login time. Add this to the end of ~/.zshrc:
-source ~/.iterm2_shell_integration.zsh
+curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
+
