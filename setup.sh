6da8ff7c8daede67b5375e48dbc7732870a7b22f
Derek Braid <dbshop@dbshops-MacBook-Pro.local>
2022-01-26 15:53:06 -0500

    use zsh config over bash

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 # ln -sf ~/dotfiles/zshrc ~/.zshrc
 cat ~/dotfiles/zshrc >> ~/.zshrc
 
-curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
-source ~/.iterm2_shell_integration.bash
-
+curl -L https://iterm2.com/shell_integration/zsh \
+-o ~/.iterm2_shell_integration.zsh
+# Next, you need to load the script at login time. Add this to the end of ~/.zshrc:
+source ~/.iterm2_shell_integration.zsh
