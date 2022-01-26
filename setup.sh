72868b386496422761f4aed8c846a4e8057934d0
Derek Braid <dbshop@dbshops-MacBook-Pro.local>
2022-01-26 15:42:57 -0500

    add shell integration to iTerm #YOLO

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 # ln -sf ~/dotfiles/zshrc ~/.zshrc
 cat ~/dotfiles/zshrc >> ~/.zshrc
+
+curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
+source ~/.iterm2_shell_integration.bash
+
