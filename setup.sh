e2751c3d117abeaffc38a85eeac3e181f57f3190
Derek Braid <dbshop@dbshops-MacBook-Pro.local>
2022-01-26 16:39:29 -0500

    set hostname dynamically

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # ln -sf ~/dotfiles/zshrc ~/.zshrc
 cat ~/dotfiles/zshrc >> ~/.zshrc
 # enable command+click on files via remote shell    
-export iterm2_hostname=spin
+export iterm2_hostname=$(spin info fqdn)
 curl -L https://iterm2.com/shell_integration/zsh -o ~/.iterm2_shell_integration.zsh
 
